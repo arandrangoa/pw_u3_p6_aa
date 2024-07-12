c8219fe8ca9c1c28a635ea177d427562c3ae2600
Alex Andrango <arandrangoa@uce.edu.ec>
2024-07-12 17:29:27 -0500

    Tarea 8

--- src/main/java/com/edu/uce/pw/api/controller/MateriaController.java
+++ src/main/java/com/edu/uce/pw/api/controller/MateriaController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,73 +25,76 @@ import com.edu.uce.pw.api.service.IMateriaService;
 @RequestMapping(path = "/materias")
 public class MateriaController {
 
-    @Autowired
-    private IMateriaService iMateriaService;
+        @Autowired
+        private IMateriaService iMateriaService;
 
-    @PostMapping(path = "/guardar")
-    public ResponseEntity<Materia> guardar(@RequestBody Materia mat) {
-        this.iMateriaService.guardar(mat);
-        
-		HttpHeaders cabeceraPost= new HttpHeaders();
-		cabeceraPost.add("mensaje_201", "Corresponde a la inserción de un recurso");
-		cabeceraPost.add("valor", "Materia ingresada con éxito");
-		return new ResponseEntity<>(mat ,cabeceraPost,201); 
+        @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+        public ResponseEntity<Materia> guardar(@RequestBody Materia mat) {
+            this.iMateriaService.guardar(mat);
+            
+            HttpHeaders cabeceraPost = new HttpHeaders();
+            cabeceraPost.add("mensaje_201", "Corresponde a la inserción de un recurso");
+            cabeceraPost.add("valor", "Materia ingresada con éxito");
+            return new ResponseEntity<>(mat, cabeceraPost, 201);
+        }
 
+        @PutMapping(path = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
+        public ResponseEntity<Materia> actualizar(@PathVariable Integer id, @RequestBody Materia mat) {
+            mat.setId(id);
+            this.iMateriaService.actualizar(mat);
+            
+            HttpHeaders cabeceraPut = new HttpHeaders();
+            cabeceraPut.add("mensaje_239", "Corresponde a la actualización de un recurso");
+            cabeceraPut.add("valor", "Materia actualizada parcialmente");
+            return new ResponseEntity<>(mat, cabeceraPut, 239);
+        }
+
+        @DeleteMapping(path = "/{id}")
+        public ResponseEntity<String> borrar(@PathVariable Integer id) {
+            this.iMateriaService.borrar(id);
+            
+            HttpHeaders cabeceraDelete = new HttpHeaders();
+            cabeceraDelete.add("mensaje_240", "Corresponde a la eliminación del recurso");
+            cabeceraDelete.add("valor", "Materia eliminada");
+            return new ResponseEntity<>("Eliminado correctamente", cabeceraDelete, 240);
+        }
+
+        @PatchMapping(path = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
+        public ResponseEntity<Materia> actualizarParcial(@RequestBody Materia mat, @PathVariable Integer id) {
+            mat.setId(id);
+            Materia mat2 = this.iMateriaService.buscar(mat.getId());
+            
+            if (mat.getNombreMateria() != null) {
+                mat2.setNombreMateria(mat.getNombreMateria());
+            }
+            if (mat.getNumCreditos() != null) {
+                mat2.setNumCreditos(mat.getNumCreditos());
+            }
+            if (mat.getNumEstudiantes() != null) {
+                mat2.setNumEstudiantes(mat.getNumEstudiantes());
+            }
+            if (mat.getProfesor() != null) {
+                mat2.setProfesor(mat.getProfesor());
+            }
+            
+            HttpHeaders cabeceraPatch = new HttpHeaders();
+            cabeceraPatch.add("mensaje_239", "Corresponde a la actualización parcial de un recurso");
+            cabeceraPatch.add("valor", "Materia actualizada parcialmente");
+            
+            this.iMateriaService.actualizar(mat2);
+            return new ResponseEntity<>(mat2, cabeceraPatch, 239);
+        }
+
+        @GetMapping(path = "/{id}")
+        public ResponseEntity<Materia> buscarPorId(@PathVariable Integer id) {
+            HttpHeaders cabeceras = new HttpHeaders();
+            cabeceras.add("mensaje_236", "Corresponde a la consulta de un recurso");
+            cabeceras.add("valor", "Materia encontrada");
+            return new ResponseEntity<>(this.iMateriaService.buscar(id), cabeceras, 236);
+        }
     }
 
-    @PutMapping(path = "/actualizar")
-    public ResponseEntity<Materia> actualizar(@RequestBody Materia mat) {
-        this.iMateriaService.actualizar(mat);
-        HttpHeaders cabeceraPatch= new HttpHeaders();
-		cabeceraPatch.add("mensaje_239", "Corresponde a la actualización parcial de un recurso");
-		cabeceraPatch.add("valor", "Materia actualizado parcialmente");
-		//return ResponseEntity.status(239).body(est2);
-		return new ResponseEntity<>(mat,cabeceraPatch,239);
-    }
-
-    @DeleteMapping(path = "/borrar/{id}")
-    public ResponseEntity<String> borrar(@PathVariable Integer id) {
-        this.iMateriaService.borrar(id);
-        
-        HttpHeaders cabeceraDelete= new  HttpHeaders();
-		cabeceraDelete.add("mensaje_240", "Corresponde a la eliminación del recurso");
-		cabeceraDelete.add("valor", "Materia eliminada");
-		return new ResponseEntity<>("Eliminado correctamente",cabeceraDelete,240);
-    }
-
-    @PatchMapping(path = "/{id}")
-    public ResponseEntity<Materia> actualizarParcial(@RequestBody Materia mat, @PathVariable Integer id) {
-    	mat.setId(id);
-		Materia mat2=this.iMateriaService.buscar(mat.getId());
-		if(mat.getNombreMateria()!=null) {
-			mat2.setNombreMateria(mat.getNombreMateria());
-		}
-		if(mat.getNumCreditos()!=null) {
-			mat2.setNumCreditos(mat.getNumCreditos());
-		}
-		if(mat.getNumEstudiantes()!=null) {
-			mat2.setNumEstudiantes(mat.getNumEstudiantes());
-		}
-		if(mat.getProfesor()!=null) {
-			mat2.setProfesor(mat.getProfesor());
-		}
-		HttpHeaders cabeceraPut= new  HttpHeaders();
-		cabeceraPut.add("mensaje_240", "Corresponde a la eliminación del recurso");
-		cabeceraPut.add("valor", "Estudiante eliminado");
-		this.iMateriaService.actualizar(mat2);
-		return new ResponseEntity<>(mat,cabeceraPut,239);
-    }
-    
-	@GetMapping(path = "/{id}",produces="application/xml")
-	public ResponseEntity<Materia> buscarPorId(@PathVariable Integer id) {
-		// return ResponseEntity.status(236).body(this.estudianteService.buscar(id));
-		HttpHeaders cabeceras = new HttpHeaders();
-		cabeceras.add("mensaje_236", "Corresponde a la consulta de un recurso");
-		cabeceras.add("valor", "Materia encontrada");
-		return new ResponseEntity<>(this.iMateriaService.buscar(id), cabeceras, 236);
-	}
 	
 	
 	
     
-}
